3bacac060992cd2d27507581522f5e0b4fa61a8e
elhombre <elhombre.dev@gmail.com>
2022-04-08 22:16:46 +0500

    Make source code conform to eslint rules

--- examples/test_event_emitter.ts
+++ examples/test_event_emitter.ts
@@ -1,26 +1,26 @@
-import { Constructor } from '../../ts-goodies/dist'
+import { Constructor } from 'type-fest'
 import { EventEmitterMixin, IBaseEvents } from '../lib'
 
 interface IMessageEvents extends IBaseEvents {
-  error: (error: Error) => void
-  message: (body: string, from: string) => void
+    error: (error: Error) => void
+    message: (body: string, from: string) => void
 }
 
 class BaseEmitter {
-  constructor() {
-    console.log('Base constructor')
-  }
+
+    constructor () {
+        console.log('Base constructor')
+    }
+
 }
 
-class MessageEmitter extends EventEmitterMixin<IMessageEvents, Constructor<BaseEmitter>>(
-  BaseEmitter
-) {}
+class MessageEmitter extends EventEmitterMixin<IMessageEvents, Constructor<BaseEmitter>>(BaseEmitter) {}
 
 const m = new MessageEmitter()
 
 m.on('message', (body: string, from: string) => {
-  console.log('Event:', body, from)
+    console.log('Event:', body, from)
 })
 
-m.emit('message', 'Hi there!', 'no-reply@test.com')
-m.emit('message', 'Hi there!', 'ook')
+m.emit('message', 'Hi there!', 'This is a first message')
+m.emit('message', 'Hi there!', 'This is a second message')
--- lib/typed_event_emitter.ts
+++ lib/typed_event_emitter.ts
@@ -3,32 +3,35 @@
  * Interface declaration borrowed from:
  * https://github.com/andywer/typed-emitter
  */
-export type EventEmitArgs<T> = [T] extends [(...args: infer U) => any]
-  ? U
-  : [T] extends [void]
-  ? []
-  : [T]
+export type EventEmitArgs<T> = [T] extends [(...args: infer U) => unknown]
+    ? U
+    : [T] extends [void]
+        ? []
+        : [T]
 
+// eslint-disable-next-line
 export interface IBaseEvents {}
+
+export type EventListener = (...args: unknown[]) => void
 
 export type EventKeyType = string | symbol
 
-export interface ITypedEventEmitter<Events extends IBaseEvents = {}> {
-  addListener<E extends keyof Events>(
-    event: E,
-    listener: Events[E],
-    counter: number
-  ): this
-  emit<E extends keyof Events>(
-    event: E,
-    ...args: EventEmitArgs<Events[E]>
-  ): boolean
-  eventNames(): EventKeyType[]
-  listenerCount<E extends keyof Events>(event: E): number
-  listeners<E extends keyof Events>(event: E): Function[]
-  off<E extends keyof Events>(event: E, listener: Events[E]): this
-  on<E extends keyof Events>(event: E, listener: Events[E]): this
-  once<E extends keyof Events>(event: E, listener: Events[E]): this
-  removeAllListeners<E extends keyof Events>(event?: E): this
-  removeListener<E extends keyof Events>(event: E, listener: Events[E]): this
+export interface ITypedEventEmitter<Events extends IBaseEvents = IBaseEvents> {
+    addListener<E extends keyof Events>(
+        event: E,
+        listener: Events[E],
+        counter: number
+    ): this
+    emit<E extends keyof Events>(
+        event: E,
+        ...args: EventEmitArgs<Events[E]>
+    ): boolean
+    eventNames(): EventKeyType[]
+    listenerCount<E extends keyof Events>(event: E): number
+    listeners<E extends keyof Events>(event: E): EventListener[]
+    off<E extends keyof Events>(event: E, listener: Events[E]): this
+    on<E extends keyof Events>(event: E, listener: Events[E]): this
+    once<E extends keyof Events>(event: E, listener: Events[E]): this
+    removeAllListeners<E extends keyof Events>(event?: E): this
+    removeListener<E extends keyof Events>(event: E, listener: Events[E]): this
 }
